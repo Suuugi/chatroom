fbc35476d0aef626b16a7a061f36d093b1b8962a
Nicholas Sugimoto <nlsugi10@gmail.com>
2023-10-28 15:46:59 -0700

    FIX message scroll and message submittal to clear content after message

--- static/js/client.js
+++ static/js/client.js
@@ -2,19 +2,24 @@ const sio = io()
 
 const ul = document.getElementById('chatwindow')
 
+
 sio.on('connected', () => {
   console.log('Client user connected to server')
 })
 
 function sendMessage() {
-  message = document.getElementById('message').value
+  var message = document.getElementById('message').value
   sio.emit('message', message)
+
+  document.getElementById('message').value = ""
 }
 sio.on('messaged', (message) => {
   var li = document.createElement("li")
 
   li.appendChild(document.createTextNode(message))
   ul.appendChild(li)
+
+  ul.scrollTop = ul.scrollHeight;
 
   console.log('Client user received message from server: ', message)
 })
